d6a344dab5d67a31405275a0fb7fd8d9ec088d26
Thales Avila <thalespizza@gmail.com>
2019-03-26 09:22:25 -0300

    Transformando UserHeader em stateless component

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class UserHeader extends React.Component {
-  render() {
-    const { user } = this.props;
-    if (!user) {
-      return null;
-    }
-    return <div className="header">{this.props.user.name}</div>;
+const UserHeader = props => {
+  const { user } = props;
+  if (!user) {
+    return null;
   }
-}
+  return <div className="header">{props.user.name}</div>;
+};
 
 const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
